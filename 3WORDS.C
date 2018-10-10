2f44051823c9f557fbcf681d61dbf5aedef36429
Adarsh Balachandran <42461327+adarshbalu@users.noreply.github.com>
2018-10-10 20:43:45 +0530

    Enter 3 words and Display them

---
+++ 3WORDS.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char w1[10],w2[10],w3[10];
+clrscr();
+printf(" enter text:");
+gets(w1);
+gets(w2);
+gets(w3);
+printf("\nword 1 is %s \n",w1);
+printf("word 2 is %s \n",w2);
+printf("word 3 is %s \n",w3);
+getch();
+}
+
