2418146c0bd6d1122a562435cb7adb2c734b1062
Adarsh Balachandran <42461327+adarshbalu@users.noreply.github.com>
2018-10-10 20:46:08 +0530

    Program to substract 2 numbers

---
+++ ADDIFF.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int a,b,diff;
+	printf("Enter two numbers ");
+	scanf("%d%d",&a,&b);
+	diff=a-b;
+	printf("The diffrence is %d ",diff);
+	getch();
+}
