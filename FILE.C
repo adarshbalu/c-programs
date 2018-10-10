66791ac4ee3764d102dc462ed6fdc94aa925ccf9
Adarsh Balachandran <42461327+adarshbalu@users.noreply.github.com>
2018-10-10 23:06:21 +0530

    Demonstrate the use of File in C

---
+++ FILE.C
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	FILE *f1,*f2,*f3;
+	int num,i;
+	clrscr();
+	printf("Enter content : ");
+	f1=fopen("DATA","w");
+	for(i=1;i<=10;i++)
+	{
+		scanf("%d",&num);
+		if(num==-1)
+			break;
+		putw(num,f1);
+	}
+	fclose(f1);
+	f1=fopen("DATA","r");
+	f2=fopen("ODD","w");
+	f3=fopen("EVEN","w");
+	while((num=getw(f1))!=EOF)
+	{
+		if(num%2==0)
+			putw(num,f3);
+		else
+			putw(num,f2);
+
+	}
+	fclose(f1);
+	fclose(f2);
+	fclose(f3);
+	f2=fopen("ODD","r");
+	f3=fopen("EVEN","r");
+	printf("\nContents of odd file : ");
+	while((num=getw(f2))!=EOF)
+		printf("%d\t",num);
+	printf("\nContents of Even file : ");
+	while((num=getw(f3))!=EOF)
+		printf("%d\t",num);
+	fclose(f2);
+	fclose(f3);
+	getch();
+
+
+
+
+}
