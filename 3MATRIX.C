302c965c2e8caebab736f67e9ceaa0bc60bf6c11
Adarsh Balachandran <42461327+adarshbalu@users.noreply.github.com>
2018-10-10 20:39:44 +0530

    Simple 3*3 Matrix Program
    Program to accept values to a 3*3 matrix and print them in matrix form

---
+++ 3MATRIX.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[3][3],i,j;
+	clrscr();
+	printf("Enter array Elements : ");
+	for(i=0;i<3;i++)
+		for(j=0;j<3;j++)
+			scanf("%d",&a[i][j]);
+	printf("\nThe Matrix is : ");
+	for(i=0;i<3;i++)
+	{
+		printf("\n");
+		for(j=0;j<3;j++)
+			printf("\t%d",a[i][j]);
+	}
+	getch();
+}
