21b94d24d2a9e363df3bcf91b1957d6eec172683
Adarsh Balachandran <42461327+adarshbalu@users.noreply.github.com>
2018-10-10 20:51:12 +0530

    Program to find sum of numbers

---
+++ ADSUM.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int a,b,sum;
+	printf("Enter two numbers");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("The sum is %d",sum);
+	getch();
+}
