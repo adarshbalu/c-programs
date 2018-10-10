bb6a9ab813ca4bf2efe0822dc5429d8e0f11328f
Adarsh Balachandran <42461327+adarshbalu@users.noreply.github.com>
2018-10-10 20:55:01 +0530

    Program to find area of Rectangle

---
+++ AREARECT.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,area;
+	clrscr();
+	printf("Enter the Lenght and Breadth : ");
+	scanf("%d%d",&a,&b);
+	area=a*b;
+	printf("\nArea is %d: ",area);
+	getch();
+}
